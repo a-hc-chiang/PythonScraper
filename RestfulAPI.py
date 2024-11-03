b8db64b91a307c6b648f58bcdb26c5807a484675
Angela Chiang <achi2048@students.cs.ubc.ca>
2024-11-02 21:33:12 -0700

    Create account backend complete

--- RestfulAPI.py
+++ RestfulAPI.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, request
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import apikeys
+from datetime import datetime
 
 # Connect to PostgreSQL 
 conn = psycopg2.connect(
@@ -25,14 +26,13 @@ searchResult = ""
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/series', methods=["GET"]) #endpoint
 
+@app.route('/series', methods=["GET"]) #endpoint
 def get_series():
     results = cur.execute("SELECT * FROM SERIES")
     return cur.fetchall(), 200
 
 @app.route('/search', methods=["GET"]) #endpoint
-
 def get_series_with_keyword_and_tag():
     titleArg = request.args.get("q", type=str)
     tagArg = request.args.get("tag", type=str)
@@ -66,6 +66,33 @@ def get_series_with_keyword_and_tag():
                 results.sort(reverse=True, key=lambda x: x["rating"])     
     return results, 200
 
+@app.route('/signup', methods=["POST"]) #endpoint
+def create_user(): # creating a user + adding account to database 
+    #accountId, accountname, email, creationdate, password, profilepic, birthday
+    #unique values - accountname + email 
+
+    # getting valid information 
+    jsonRequest = request.get_json() # returns a dictionary 
+
+    # putting insert statement together
+    accountId = cur.execute("SELECT * FROM account")
+    accountID = len(cur.fetchall()) + 1
+    accountName = request.get_json().get('accountname', '')
+    email = jsonRequest.get('email', '')
+    currDate = str(datetime.today())
+    currDate = currDate[0:10]
+    password = jsonRequest.get('password', '')
+    profilepic = ""
+    birthday = jsonRequest.get('birthday', '')
+
+    together = "(" + str(accountID) + ", '" + str(accountName) + "', '" + str(email) + "', '" + currDate + "', '" + password + "', '" + profilepic + "', '" + str(birthday) + "')"
+
+    # executing insert statement
+    cur.execute("INSERT INTO account (accountId, accountname, email, creationdate, password, profilepic, birthday) VALUES" + together)
+
+    # commit changes to database 
+    conn.commit()
+    return {"status":"success"}, 200
 
 if __name__ == '__main__': 
     app.run(debug = True)
